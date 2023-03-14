03b9a2594f42c3b096956c555df34f62009f6f65
paul-unifra <paul@unifra.io>
2023-03-14 10:56:18 +0800

    add sample repo

---
+++ sample/README.md
@@ -0,0 +1,15 @@
+# Sample
+
+This is a sample repo for unifra bundle ethclient.
+
+## Build
+
+```bash
+go build
+```
+
+## Run
+
+```bash
+./sample
+```
---
+++ sample/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/unifra20/ethclient"
+)
+
+func main() {
+	_unifraBundleAPI := "https://eth-mainnet.unifra.io/v1/f7530c7a69314a6da8c430d96f10de64"
+	ec, _ := ethclient.Dial(_unifraBundleAPI)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	// BlockByNumber
+	bn := big.NewInt(10000000)
+	block, err := ec.BlockByNumber(ctx, bn)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("BlockByNumber Hash: %s\n", block.Hash().Hex())
+
+	// HeaderByNumber
+	header, err := ec.HeaderByNumber(ctx, bn)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("HeaderByNumber Hash: %s\n", header.Hash().Hex())
+
+	// BlockReceipts
+	receipts, err := ec.BlockReceiptsByNumber(ctx, bn)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("BlockReceiptsByNumber Frist Hash: %s\n", receipts[0].BlockHash.Hex())
+
+	// BlockTraceByNumber
+	btrace, err := ec.BlockTraceByNumber(ctx, bn)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("BlockTraceByNumber Frist Hash: %s\n", btrace[0].BlockHash.Hex())
+
+	// TransactionTraceByHash
+	txtrace, err := ec.TransactionTraceByHash(ctx, *btrace[0].TransactionHash)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("TransactionTraceByHash Frist Hash: %s\n", txtrace[0].BlockHash.Hex())
+}
